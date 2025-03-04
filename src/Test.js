0675cf3e873100e256e34a41c0c58a6ea2fa9257
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-04 21:17:01 +0100

    create function buttonHandler

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 class Test extends React.Component{
+    
+    buttonHandler = () => {
+        console.log('buttonHandler - worked');
+        
+    }
 
+     render() {
+ console.log('render-1');
+ 
+    return (
+        <div>
+            <div>
+                <button onClick={this.buttonHandler} > Push</button>
+            </div>
+        </div>
+    )
+     }
 }
